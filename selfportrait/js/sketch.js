a4651704a078a290ef29e0b2f32a7fae0e762571
flili <lilij568@gmail.com>
2017-02-14 14:42:44 -0500

    Add files via upload
    selfportrait

--- selfportrait/js/sketch.js
+++ selfportrait/js/sketch.js
@@ -1,18 +1,46 @@
 function setup() {
     //use code to create and draw on browser
     //createCanvas(width, height)
-    createCanvas(640, 600);
+    createCanvas(700, 700);
     
-    background('lightgray');
+    background(170, 206, 226);
     
-    ellipse(300, 300, 400);
+    //two points, numbers in (x1, y1 ; x2, y2)
     
-    rect(175, 175, 50, 50);
-    rect(275, 175, 50, 50);
+    //ellipse right-left,up-down, width, height
+    //head
+    fill(256, 220, 177)
+    ellipse(300,400,400,550);
     
-    line(250, 245, 200, 350);
+    //eyes
+     fill('white')
+    ellipse(200, 350, 150, 50);
+    ellipse(400, 350, 150, 50);
+   
+    //pupils
+     ellipseMode(RADIUS);  // Set ellipseMode to RADIUS
+    fill('black');
+    ellipse(200, 350, 20, 20);
+    ellipse(400, 350, 20, 20);
+    //brow
+    triangle(120, 300, 275, 270, 280, 300);
+    triangle(320, 300, 325, 270, 480, 300);
     
-    triangle(175, 400, 325, 400, 345, 380);
+    //nose
+    fill('orange')
+    triangle(275, 450, 300, 480, 300, 380);
     
-    arc (450, 280, 70, 90, PI+1, HALF_PI+1);
+    // mouth
+    fill('red')
+    arc(300, 520, 80, 60, 0, PI);
+    
+    
+    
+    //hair
+   fill('black')
+    rect (50, 250, 85, 550);
+    rect (465, 250, 85, 550);
+     arc(300, 250, 250, 150, -PI, 0);
 }
+    
+

656d7f5a745cb5c0970bbe10fa648ae7e1d9f6c2
flili <lilij568@gmail.com>
2017-02-28 15:40:34 -0500

    add logo

--- assignmentLogo/js/sketch.js
+++ assignmentLogo/js/sketch.js
@@ -1,27 +1,34 @@
 function setup() {
     //use code to create and draw on browser
     //createCanvas(width, height)
-    createCanvas(700, 700);
-    background("lightblue");
+    createCanvas(700, 500);
+    background(255);
     
     
+    var lines = color(100,149,237);
     
     noStroke();
-    arc(180, 250, 50, 50, -PI, 0);
-    arc(230, 250, 80, 80, -PI, 0);
-    arc(300, 250, 100, 100, -PI, 0);
-    arc(350, 250, 50, 50, -PI, 0);
-    
-    //plane
-    noFill();
-    stroke(0);
-    triangle(250, 150, 500, 300, 400, 170);
-    
-    
+    rect(275,150,220,150);
+    triangle(275,150,240,300,400,300);
+    triangle(495,300,440,150,535,150);
+    stroke(lines);
+    strokeWeight(8);
+    line(275,150,240,300);
+    line(240,300,495,300);
+    line(495,300,535,150);
+    triangle(280,150,528,150,380,250);
+    line(80,150,260,150);
+    line(110,200,250,200);
+    line(140,250,240,250);
+    line(110,300,220,300);
     //TEXT
-    fill(0);
-    textSize(16);
-    text("FLY", 300, 250);
+    fill("white");
+    strokeWeight(10);
+    stroke(lines);
+    textSize(70);
+    textFont("Arial");
+    textStyle(ITALIC);
+    text("MAIL", 350, 400);
     
     
 }
--- week-02/js/sketch.js
+++ week-02/js/sketch.js
@@ -1,29 +1,39 @@
 function setup() {
     //use code to create and draw on browser
     //createCanvas(width, height)
-    createCanvas(640, 480);
+   createCanvas(700, 500);
+    background("lightblue");
     
-    background('green');
-    
-    
-    fill('blue');
-    ellipse(50, 50, 50);
-    
-    fill('#f4d85d');
-    arc(150, 50, 50, 75, 5, PI + 1 );
-    
-    strokeWeight(10);
-    stroke(200, 0, 100);
-    line(50, 50, 150, 50);
-    
-    strokeWeight(2);
-    quad(75, 75, 150, 150, 80, 180, 50, 65);
+    //teapot
+    //sides,up-down
+    triangle (150,200,225,225,225,275);
+    //
+    ellipse (490,250,70,80);
+    fill("lightblue");
+    ellipse(490,250,40,50);
+    //
+    fill(255);
+    ellipse(350,250,275,175);
+    arc(350, 180, 200, 100, -PI, 0);
+    ellipse(350,125,20,20);
     
     
-    noStroke();
-    rect(0, 240, 640,480);
     
-    stroke(200, 0,100);
-    noFill();
-    triangle(40, 90, 33, 66, 480, 275);
+    //cupcake
+    triangle(390,300,410,350,450,300);
+    triangle(510,300,490,350,450,300);
+    rect(410,300,80,50);
+    //
+    arc(450, 300, 100, 80, -PI, 0);
+    fill(255);
+    //
+    line(450,260,460,240);
+    ellipse(450,260,20,20);
+    
+    
+    
+    //TEXT
+    //fill(0);
+    //textSize(16);
+    //text("FLY", 300, 250);
 }

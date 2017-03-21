29d65702399c94a0c403948e8422d7bd6d003c21
flili <lilij568@gmail.com>
2017-03-21 13:54:08 -0500

    add code on assigment 5

--- windows/js/sketch.js
+++ windows/js/sketch.js
@@ -7,25 +7,29 @@ function setup() {
 function draw() {
 //Building background color 
     background(51, 63, 55);
-// 6 windows inline
+// 3 windows inline width/3 = 246.6
     var windowWidth = width/3; // window width
-// 4 windows down
+// 4 windows down height/4 = 625
     var windowHeight = height/4; // window height
     
     for ( var x = windowWidth/4; x < width; x+=windowWidth) {
     for (var y = windowHeight/4; y < height; y+=windowHeight) {
            
-           fill(20, 31, 25);
-           rect(x, y, windowWidth/2, windowHeight/2);
+        fill(20, 31, 25);
+        rect(x, y, windowWidth/2, windowHeight/2);
 //shadow top curtain window
-           fill(131,67,73);
-           
-           rect(x, y, windowWidth/2, windowHeight/8);
+        fill(131,67,73);
+        rect(x, y, windowWidth/2, windowHeight/8);
 //curtain widow
-           fill(186,82,76);
-           rect(x, y + windowHeight/8, windowWidth/2, windowHeight/4);
-           rect(x + windowWidth/4, y + windowHeight/8, windowWidth/4, windowHeight/8);
-           rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);
+        fill(186,82,76);
+        rect(x, y + windowHeight/8, windowWidth/2, windowHeight/4);
+//curtain shadow down
+        fill(131,67,73);
+        noStroke();
+        rect(x, y+windowHeight/3.5, windowWidth/2, windowHeight/10);
+//marco
+        noFill();
+           //rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);
 //top window
            
            

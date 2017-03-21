a343ae233b2c56292926582981d256ba3fee6516
flili <fane.lili@f706-13-34151.bmcc.cuny.edu>
2017-03-21 16:26:20 -0400

    windows.html

--- windows/js/sketch.js
+++ windows/js/sketch.js
@@ -12,35 +12,67 @@ function draw() {
 // 4 windows down height/4 = 625
     var windowHeight = height/4; // window height
     
+    
+//colors
+    //light Green
+    var lg = color (143, 151, 123);
+    //dark Green
+    var dg = color (20, 31, 25);
+    //Dark Orange
+    var dn = color (131,67,73);
+    //light Orange
+    var lo = color (186,82,76);
+    
+    
     for ( var x = windowWidth/4; x < width; x+=windowWidth) {
     for (var y = windowHeight/4; y < height; y+=windowHeight) {
         noStroke();
         fill(20, 31, 25);
         rect(x, y, windowWidth/2, windowHeight/2);
 //shadow top curtain window
-        fill(131,67,73);
+        fill(dn);
         rect(x, y, windowWidth/2, windowHeight/8);
 //curtain widow
-        fill(186,82,76);
+        fill(lo);
         rect(x, y + windowHeight/8, windowWidth/2, windowHeight/4);
 //curtain shadow down
-        fill(131,67,73);
+        fill(dn);
         noStroke();
         rect(x, y+windowHeight/3.5, windowWidth/2, windowHeight/10);
 //marco
-        stroke(0);
+        stroke(dg);
+        strokeWeight(8);
         noFill();
+//marco back
+           rect(x + windowWidth/y, y + windowHeight/13, windowWidth/2, windowHeight/2.5);
+
+//marco half
+        stroke(dg);
         noFill();
-        strokeWeight(4)
-           rect(x + windowWidth/500, y + windowHeight/16 * 2, windowWidth/2, windowHeight/4);
+        rect(x + windowWidth/y, y + windowHeight/13, windowWidth/2, windowHeight/4.7);
+        
 //top window
-        fill(175,181,146);
-        line(x, windowWidth/2, windowHeight/2);
         noStroke();
-        fill(20, 31, 25);
-        rect(x-20, y-20, windowWidth/1.5, windowHeight/10,x);
-           
-           
+        fill(dg);
+        rect(x-20, y+5, windowWidth/1.5, windowHeight/15,x);
+        
+        rect(x-33, y, windowWidth/1.3, windowHeight/25);
+        
+//Bottom Window
+        
+        fill(dg);
+        noStroke();
+        rect(x-20, y+300, windowWidth/1.5, windowHeight/50);
+        rect(x-20, y+300, windowWidth/1.5, windowHeight/30,x);
+        
+        
+        rect(x-10, y+300, windowWidth/1.7, windowHeight/18,x);
+        
+        rect(x-2, y+330, windowWidth/1.9, windowHeight/50);
+//shadows
+        stroke(lg);
+        line 
+         
         }
     }
         

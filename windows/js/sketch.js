dbb6171652f77aeb6568da4bf4c8d3e6b999123a
flili <fane.lili@f706-13-34151.bmcc.cuny.edu>
2017-03-21 17:50:31 -0400

    widow shadows and edit index.html from windows

--- windows/js/sketch.js
+++ windows/js/sketch.js
@@ -28,60 +28,99 @@ function draw() {
     
     for ( var x = windowWidth/4; x < width; x+=windowWidth) {
     for (var y = windowHeight/4; y < height; y+=windowHeight) {
+        
+        
         noStroke();
         fill(20, 31, 25);
         rect(x, y, windowWidth/2, windowHeight/2);
+
 //shadow top curtain window
         fill(dn);
         rect(x, y, windowWidth/2, windowHeight/8);
+        
 //curtain widow
         fill(lo);
         rect(x, y + windowHeight/9, windowWidth/2, windowHeight/4);
+        
 //curtain shadow down
         fill(dn);
         noStroke();
         rect(x, y+windowHeight/3.5, windowWidth/2, windowHeight/10);
+        
 //marco
-        stroke(dg);
-        strokeWeight(8);
-        noFill();
+        
+        
+        
 //marco back
-           rect(x + windowWidth/y, y + windowHeight/13, windowWidth/2, windowHeight/2.5);
-
+    //orange shadow outside
+        stroke(dn);
+        noFill(0);
+        rect(x + windowWidth/y, y + windowHeight/12, windowWidth/2+2, windowHeight/2.5);
+        
+    //light green shadow inside
+        noFill();
+        stroke(lg);
+        strokeWeight(2);
+        rect(x-2.1 + windowWidth/y, y + windowHeight/12, windowWidth/2-5, windowHeight/2.5);
+           
+        stroke(dg);
+        strokeWeight(12);
+        rect(x + windowWidth/y, y + windowHeight/15, windowWidth/2, windowHeight/2.5);
 //marco half
+        strokeWeight(12);
         stroke(dg);
         noFill();
         rect(x + windowWidth/y, y + windowHeight/13, windowWidth/2, windowHeight/4.7);
         
 //top window
-    //shadow
         noStroke();
+        
+    //light green shadow up outside
         fill(lg);
-        
         rect(x-33, y-2, windowWidth/1.3, windowHeight/25);
-        
+    //dark
         fill(dg);
+        
         rect(x-20, y+5, windowWidth/1.5, windowHeight/15,x);
         
         rect(x-34, y, windowWidth/1.28, windowHeight/25);
         
-    //
+    //actual window
+        
+        fill(g);
+        rect(x-20, y+15, windowWidth/1.5, windowHeight/30,x);
+        
+        rect(x-24, y, windowWidth/1.28, windowHeight/25);
         
         
 //Bottom Window
+    //light green shadow window down
+        fill(lg);
+        rect(x-20, y+298, windowWidth/1.5, windowHeight/50);
         
+    //dark shadow
         fill(dg);
         noStroke();
-        rect(x-20, y+300, windowWidth/1.5, windowHeight/50);
-        rect(x-20, y+300, windowWidth/1.5, windowHeight/30,x);
+        
+        rect(x-21, y+300, windowWidth/1.48, windowHeight/50);
+        
+        rect(x-21, y+300, windowWidth/1.48, windowHeight/25,x);
         
         
-        rect(x-10, y+300, windowWidth/1.7, windowHeight/18,x);
+        rect(x-10, y+300, windowWidth/1.7, windowHeight/15,x);
         
         rect(x-2, y+330, windowWidth/1.9, windowHeight/50);
-//shadows
-        stroke(lg);
-        line 
+        
+    //actual window
+        fill(g);
+        rect(x-22, y+299.9, windowWidth/1.46, windowHeight/60);
+        
+        rect(x-15, y+315, windowWidth/1.60, windowHeight/100,x);
+        
+        rect(x-2, y+325, windowWidth/1.9, windowHeight/65);
+        
+        
+        
          
         }
     }

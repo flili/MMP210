603a7ebb81a63da10c4a9fcc7b4404c1b657964b
flili <fane.lili@f706-14-34149.bmcc.cuny.edu>
2017-04-04 16:40:43 -0400

    assigment 2 variables

--- assignment2/js/sketch.js
+++ assignment2/js/sketch.js
@@ -1,40 +1,79 @@
 function setup() {
     //use code to create and draw on browser
     //createCanvas(width, height)
-   createCanvas(700, 500);
+    createCanvas(700, 500);
     background("lightblue");
     
-    //teapot
-    //sides,up-down
-    triangle (150,200,225,225,225,275);
+//colors
+    var r = color(220,20,60);
+    var b = color(128,0,0);
+    var g = color(144,238,144);
+    var t = color(255,228,196);
+    var tb = color (139,69,19);
+    
+//values
+    var a = 150;
+    var c = 125;
+    var d = 100;
+    var e = 70;
+    var f = 80;
+    var h = 200;
+    var i = 225;
+    var j = 275;
+    var k = 250;
+    var l = 490;
+    var m = 350;
+    var n = 205;
+    var o = 20;
+    var p = 40;
+    var q = 50;
+    var s = 180;
+    var u = 450;
+    var v = 300;
+    var w = 255;
+    var x = 260;
+    var y = 460;
+    var z = 240;
+    var aa = 390;
+    var ab = 510;
+    var ac = 410;
+    var ad = 650;
+    
+    
+//teapot
+//sides,up-down
+    fill (t);
+    triangle (a,h,i,i,i,j);
     //
-    ellipse (490,250,70,80);
+    ellipse (l,k,e,f);
     fill("lightblue");
-    ellipse(490,250,40,50);
-    //
-    fill(255);
-    ellipse(350,250,275,175);
-    arc(350, 180, 200, 100, -PI, 0);
-    ellipse(350,125,20,20);
+    ellipse(l,k,p,q);
+//body teapot
+    fill(t);
+    ellipse(m,k,j,n);
+//top teapot
+    arc(m, s, h, d, -PI, 0);
+    ellipse(m,c,o,o);
     
     
     
-    //cupcake
+//cupcake
+//filling
+    fill (g);
+    arc(u, v, d, f, -PI, 0);
+    fill(w);
+//cherry
+    fill (r);
+    line(u,x,y,z);
+    ellipse(u,x,o,o);
+//bottom
+    fill (b);
+    quad (aa,v,ab,v,l,m,ac,m);
+
     
-    triangle(390,300,410,350,450,300);
-    triangle(510,300,490,350,450,300);
-    rect(410,300,80,50);
-    //
-    arc(450, 300, 100, 80, -PI, 0);
-    fill(255);
-    //
-    line(450,260,460,240);
-    ellipse(450,260,20,20);
+//table
+    stroke(tb);
+    strokeWeight(10);
+    line(q,m,ad,m);
     
-    
-    
-    //TEXT
-    //fill(0);
-    //textSize(16);
-    //text("FLY", 300, 250);
 }

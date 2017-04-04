a2a87de61eb223d3a31418c5ac96b4819bb1ea09
flili <fane.lili@f706-14-34149.bmcc.cuny.edu>
2017-04-04 15:15:10 -0400

    change

--- midterm/js/sketch.js
+++ midterm/js/sketch.js
@@ -1,18 +1,47 @@
 // windows
 function setup() { 
     createCanvas (640,640);
-    background(0);
+    background(255);
     
-    stroke ("green");
+    stroke ("white");
     
     var numberOfRectangles = 200;
     var columns = Math.sqrt(numberOfRectangles);
-    var rows = 10;
-    var size = width / columns;
+    var rows = Math.sqrt(numberOfRectangles);
+
+//
+//
+    var w = width / columns;
+    var h = height / rows;
     
-    for (var x = 0; x <= width; x += size); {
-        for ( var y = 0; y <= height; y += size);{
-        rect(x, y, size, size);
+    for (var x = 0; x <= width; x += w) {
+        for ( var y = 0; y <= height; y += h) {
+            
+            
+            var b;
+        //rain
+            if (y < height/4){
+                b = random (200, 255);
+                x = random (-25, 25);
+                w = random (10,20);
+            } else {
+                b = random (0, 100);
+            }
+            
+        //green leaves
+            fill(
+            random (0 , 50),
+            random (150, 255),
+            b
+            );
+            
+        //Flowers
+            if(y > height * 3/4) {
+                ellipse (x,y,w,h);
+            } else{
+                rect(x, y, w, h);
+            }
+            
         }
     }
 }

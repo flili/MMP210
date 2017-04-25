7b07edbbdb2fa5a9a7288929d5060e0eccaac457
flili <lilij568@gmail.com>
2017-04-25 14:29:34 -0500

    midterm

--- assignmet8Midterm/js/sketch.js
+++ assignmet8Midterm/js/sketch.js
@@ -4,15 +4,16 @@ function setup() {
     createCanvas(640, 640);
     background(255);
     
-   noStroke();
     
-    var numberOfRectangles = 200;
+    noStroke();
+    
+    var numberOfRectangles = 300;
     var columns = Math.sqrt(numberOfRectangles);
     var rows = Math.sqrt(numberOfRectangles);
     
     var w = width / columns;
     var h = height / rows;
-
+    
     for (var x = 0; x <= width; x += w) {
         for (var y = 0; y <= height; y += h) {
             
@@ -33,9 +34,9 @@ function setup() {
                 
             } else if (y > height * 3/4) {
 // random ocean color
-                r = random(0, 50);
-                g = random(150, 255);
-                b = random(200, 255);
+                r = random(206, 210);
+                g = random(139, 165);
+                b = random(84, 109);
                 
                 
 // random ocean waves size
@@ -43,9 +44,9 @@ function setup() {
                 
             } else {
 // random sand color
-                r = random(206, 210);
-                g = random(139, 165);
-                b = random(84, 109);
+                r = random(0, 50);
+                g = random(150, 255);
+                b = random(200, 255);
                 ellipse(x,y,h,w);
             }
             
@@ -55,12 +56,15 @@ function setup() {
 // oean
             if ( y > height * 3/4) {
 // ocean
-                ellipse(x, y, w, h);
+                ellipse(x-15, y-10, w+5, h-10);
 //ocean
                 fill(
+                    random(206, 210),
+                    random(139, 165),
+                    random(84, 109)/*
                     random(0, 50),
                     random(150, 255),
-                    random(200, 255)
+                    random(200, 255)*/
                 );
                 ellipse(x, y, w/3, h/3);
             } else {
@@ -73,4 +77,11 @@ function setup() {
             h = tempH;
         }
     }
+    fill(r,g,b)
+    fill(
+        random(226,255),
+        random(195,223),
+        random(17,52)
+    );
+    ellipse(200,100,100);
 }

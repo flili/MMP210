dfda1b3ea4cc0037ba54a8f783d19704bf3852f2
flili <lilij568@gmail.com>
2017-04-25 14:34:54 -0500

    coments

--- assignmet8Midterm/js/sketch.js
+++ assignmet8Midterm/js/sketch.js
@@ -22,53 +22,45 @@ function setup() {
             var tempW = w;
             var tempH = h;
             
-//palms
             if (y < height/2.5) {
-// random ocean blue color
                 r = random(229, 236);
                 g = random(121, 59);
                 b = random(27, 59);
-//random ocean size and position
                 x += random(-50, 10);
                 w = random(100, 200);
                 
             } else if (y > height * 3/4) {
-// random ocean color
                 r = random(206, 210);
                 g = random(139, 165);
                 b = random(84, 109);
                 
                 
-// random ocean waves size
+
                 w = h = random(50, 90);
                 
             } else {
-// random sand color
+
                 r = random(0, 50);
                 g = random(150, 255);
                 b = random(200, 255);
                 ellipse(x,y,h,w);
             }
             
-// all of the color fills
+
             fill(r,g, b);            
             
-// oean
+
             if ( y > height * 3/4) {
-// ocean
+
                 ellipse(x-15, y-10, w+5, h-10);
-//ocean
                 fill(
                     random(206, 210),
                     random(139, 165),
-                    random(84, 109)/*
-                    random(0, 50),
-                    random(150, 255),
-                    random(200, 255)*/
+                    random(84, 109)
+                
                 );
                 ellipse(x, y, w/3, h/3);
             } else {
-// palm and sand
                 rect(x, y, w, h);
             }
             
